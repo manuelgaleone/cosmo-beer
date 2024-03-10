f25062b4b752f94d4a3ef2c9cace327dccfb3eb5
manuelgaleone <hello@manuelgaleone.it>
2024-03-10 19:24:07 +0100

    Solved login

--- pages/login.js
+++ pages/login.js
@@ -22,50 +22,55 @@ const Login = () => {
 
   return (
     <Row className='vh-100'>
-      <div className='d-flex m-0 p-0'>
-        <div className='col-12 col-lg-7 m-0 back-beer-1 p-3 d-none d-lg-flex justify-content-center'>
+      <div className='col-12 col-lg-9 m-0 back-beer-1 p-3 d-none d-lg-flex justify-content-center'>
           <Image
             src={beerTitle}
             alt='Beer title'
             className='beer-login-title img-fluid'
           />
-        </div>
-        <div className='col-12 col-lg-5 m-0 back-accent-super-light d-flex flex-column justify-content-between align-items-center py-5'>
-          <div className='col-8 login-content p-0 m-0'>
-            <h6 className='accent pb-5 p-0 m-0 text-end'>TI DEVO UNA BIRRA</h6>
-            <h2 className='bebas-regular pt-3 m-0'>LOGIN</h2>
-            <div>
-              <Form className='login-form-container d-flex flex-column'>
-                <InputText
-                  label="Email"
-                  id="email"
-                  type="text"
-                  value={email}
-                  setValue={setEmail}
-                  placeholder="es. mario.rossi@gmail.com"
-                />
-                <InputText
-                  label="Password"
-                  id="password"
-                  type="password"
-                  value={password}
-                  setValue={setPassword}
-                  placeholder="Password"
-                />
-                <h6 className='text-end blue-light p-0 m-0 pb-3'>Password dimenticata</h6>
-                <Button className="back-accent accent-super-light border border-none">LOG IN</Button>
-              </Form>
-              <h6 className='text-center p-0 m-0 py-3'>Non hai un Account? <span className='blue-light p-0 m-0'>Registrati</span></h6>
-            </div>
-          </div>
-          <div className='col-12 login-footer d-flex align-items-center justify-content-center gap-2'>
-              <h6 className='accent p-0 m-0'>Questa birra è offerta da</h6>
-              <Image
-              alt='Cosmo logo'
-              src={cosmoAccent}
+      </div>
+      <div className='col-12 col-lg-3 m-0 back-accent-super-light d-flex flex-column justify-content-between align-items-center py-5'>
+        <div className='col-8 login-content p-0 m-0'>
+          <h6 className='accent pb-5 p-0 m-0 text-end'>TI DEVO UNA BIRRA</h6>
+          <h2 className='bebas-regular pt-3 m-0'>LOGIN</h2>
+          <div>
+            <Form className='login-form-container d-flex flex-column'>
+              <InputText
+                label="Email"
+                id="email"
+                type="text"
+                value={email}
+                setValue={setEmail}
+                placeholder="es. mario.rossi@gmail.com"
               />
-            </div>
+              <InputText
+                label="Password"
+                id="password"
+                type="password"
+                value={password}
+                setValue={setPassword}
+                placeholder="Password"
+              />
+              <h6 className='text-end blue-light p-0 m-0 pb-3'>Password dimenticata</h6>
+              <Button className="back-accent accent-super-light border border-none">LOG IN</Button>
+            </Form>
+            <h6 className='text-center p-0 m-0 py-3'>Non hai un Account? <span className='blue-light p-0 m-0'>Registrati</span></h6>
+          </div>
         </div>
+        <div className='col-12 login-footer d-flex align-items-center justify-content-center gap-2'>
+            <h6 className='accent p-0 m-0'>Questa birra è offerta da</h6>
+            <Image
+            alt='Cosmo logo'
+            src={cosmoAccent}
+            />
+          </div>
+      </div>
+      <div className='col-12 col-lg-9 m-0 back-beer-1 p-3 d-flex d-lg-none justify-content-center'>
+          <Image
+            src={beerTitle}
+            alt='Beer title'
+            className='beer-login-title img-fluid'
+          />
       </div>
     </Row>
   );
